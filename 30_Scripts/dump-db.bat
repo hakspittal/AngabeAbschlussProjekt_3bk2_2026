deb05365cadfefb6d4e93086188ddf198ef4c174
ento <105713785+codingThome@users.noreply.github.com>
2026-05-05 08:34:06 +0200

    Ausbesserung Script im Tutorial

--- 30_Scripts/dump-db.bat
+++ 30_Scripts/dump-db.bat
@@ -10,7 +10,7 @@ rem Achtung: Damit das funktioniert muss der MySQL-Server laufen!!!
 rem Hier den Pfad zu eurem mysql\bin-Ordner nehmen
 set MYSQLDUMP=F:\_schule2026\_wids3bk_2\xampp\mysql\bin\mysqldump.exe
 rem Der Output-Ordner sollte in eurem Projekt (git-repo) liegen!!!
-set output=F:\_schule2026\_wids3bk_2\dump-%TIMESTAMP%.sql
+set output=F:\_schule2026\_wids3bk_2\dump.sql
 
 "%MYSQLDUMP%" -u %DB_USER% --databases %DB_NAME% --routines --triggers --events >  %output%
 
